0ca3196ea8030144a3e9c27c375e2b3a45086436
losefor <moh999157@gmail.com>
2021-12-30 20:26:47 +0300

    init

--- libs/formik-chakra-react/jest.config.js
+++ libs/formik-chakra-react/jest.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-  displayName: 'dashboard',
+  displayName: 'formik-chakra-react',
   preset: '../../jest.preset.js',
   transform: {
-    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/dashboard',
+  coverageDirectory: '../../coverage/libs/formik-chakra-react',
 };
---
+++ libs/formik-chakra-react/src/index.tsx
@@ -0,0 +1,1 @@
+export const TextComponent = () => <p>test</p>;
